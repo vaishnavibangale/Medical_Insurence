d5f36ebad3edfb9de43db0e1e19e25a3fb6fd49b
vaishnavibangale <139327794+vaishnavibangale@users.noreply.github.com>
2023-07-25 14:52:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 import os
 
-PORT_NUMBER =8080
+PORT_NUMBER = 8080
 
 MONGODB_PORT_NUMBER = 27017
 MONGODB_DATABASE_NAME = 'project_db'
-MODEL_FILE_PATH = r'static\models\linear_regression.pkl'
 
-JSON_FILE_PATH = r'static\json_files\proj_data.json'
+# Use os.path.join to create the model and JSON file paths
+MODEL_FILE_PATH = os.path.join('static', 'models', 'linear_regression.pkl')
+JSON_FILE_PATH = os.path.join('static', 'json_files', 'proj_data.json')
